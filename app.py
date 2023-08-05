46e1429e361c3b89f79bc6d9f10db73a48bc7d65
Zzzx6h <zzzxh6@gmail.com>
2023-08-05 14:56:46 +0900

    [Initialize]

---
+++ app.py
@@ -0,0 +1,44 @@
+
+import requests
+from flask import Flask, render_template, request
+
+from src.database.conn import *
+
+@app.route('/')
+def parking_lot():
+    url = 'http://api.data.go.kr/openapi/tn_pubr_prkplce_info_api'
+
+
+    params ={'serviceKey' : 'POX/Qqea1zYsMpX+Jc69gnaFc5JQHTWhXuKwuV4MTUtTA96e7pqGxqaZBbVmeRCJYqmcu+kK46p0ZwJNifPaqw==', 'pageNo' : '1', 'numOfRows' : '100', 'type' : 'xml', 'prkplceNo' : '', 'prkplceNm' : '', 'prkplceSe' : '', 'prkplceType' : '', 'rdnmadr' : '', 'lnmadr' : '', 'prkcmprt' : '', 'feedingSe' : '', 'enforceSe' : '', 'operDay' : '', 'weekdayOperOpenHhmm' : '', 'weekdayOperColseHhmm' : '', 'satOperOperOpenHhmm' : '', 'satOperCloseHhmm' : '', 'holidayOperOpenHhmm' : '', 'holidayCloseOpenHhmm' : '', 'parkingchrgeInfo' : '', 'basicTime' : '', 'basicCharge' : '', 'addUnitTime' : '', 'addUnitCharge' : '', 'dayCmmtktAdjTime' : '', 'dayCmmtkt' : '', 'monthCmmtkt' : '', 'metpay' : '', 'spcmnt' : '', 'institutionNm' : '', 'phoneNumber' : '', 'latitude' : '', 'longitude' : '', 'referenceDate' : '', 'instt_code' : '' }
+
+
+    response = requests.get(url=url, params=params)
+    if response.status_code == 200:
+        print(response.text)
+    return render_template("index.html")
+
+# @app.route('/')
+# def T_map():
+#     version = 1
+  
+#     url = f'https://apis.openapi.sk.com/tmap/geo/reversegeocoding?version={version}&lat={lat}&lon={lon}&coordType={coordType}&addressType={addressType}&callback={callback}&appKey={appKey}'
+
+#     response = requests.get(url=url, params=params)
+#     if response.status_code == 200:
+#         print(response.text)
+#     return render_template("index.html")
+
+
+
+
+
+
+
+
+
+# 비동기는 오는 순서대로 처리함
+# 동기는 빠른거부터 처리함 => 데이터 두개가 같은 메모리를 공유하는 경우
+# => 빠른거부터 처리 => 문제가 있으면 추적이안됨
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ src/database/conn.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from sqlalchemy import Table, Column, Integer, String
+
+
+app = Flask(__name__)
+app.debug = True
+
+class db_init:
+    def __init__(self) -> None:
+        self.username = "dev"
+        self.password = "ohjihoon"
+        self.dbname = "park"
+    
+    def create_db(self):
+        app.config['SQLALCHEMY_DATABASE_URI']= f'mysql://{self.username}:{self.password}@localhost:3306/{self.dbname}'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+        db = SQLAlchemy()
+
+        db.init_app(app)
+
+        with app.app_context():
+            db.create_all()
+    
+
+
+db_init().create_db()
